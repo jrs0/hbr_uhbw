d1064d4ba810a726827cc291777cc31d094d801b
John Scott <john.scott17@nhs.net>
2024-01-29 10:08:25 +0000

    Added a few python packages to the venv

--- prototypes/test.py
+++ prototypes/test.py
@@ -1,3 +1,8 @@
+# Create VS Code virtual env (.venv) using Python 3.11.4
+# pip install numpy matplotlib pandas scikit-learn scikit-survival
 # Tested on Python 3.11.4
 import numpy as np
+import matplotlib.pyplot as plt
 import pandas as pd
+import sklearn as skl
+import sksurv as sks
