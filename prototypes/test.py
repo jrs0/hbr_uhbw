6f0c20401e59493b40a649a5cd5ffb10b62369fa
John Scott <john.scott17@nhs.net>
2024-01-29 10:31:52 +0000

    Added notes for python packages

--- prototypes/test.py
+++ prototypes/test.py
@@ -1,6 +1,14 @@
 # Create VS Code virtual env (.venv) using Python 3.11.4
+#
 # pip install numpy matplotlib pandas scikit-learn scikit-survival
-# Tested on Python 3.11.4
+#
+# Tested on Python 3.11.4.
+# 
+# Python package repositories are blocked by UHBW network policies, 
+# but installing python packages manually is allowed. It is easier 
+# to create a virtual environment, archive it, and copy it to the 
+# target computer (assuming the same python version), than to manually
+# download the dependent wheels and install them one by one.
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
