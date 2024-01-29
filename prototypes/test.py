55ef8aea76ae291475c0fb4c93f924e820df8598
John Scott <john.scott17@nhs.net>
2024-01-29 10:44:45 +0000

    Added virtual environment instructions

--- prototypes/test.py
+++ prototypes/test.py
@@ -6,6 +6,27 @@
 # target computer (assuming the same python version), than to manually
 # download the dependent wheels and install them one by one.
 #
+# On Windows, you can create a virtual environment in VS Code by
+# creating a new python file (to ensure the Python extension is
+# active), typing Ctrl-Shift-P, and running 
+#   
+#    Python: Create Environment...
+#    Venv
+#
+# This should create a folder called .venv in the current VS Code
+# folder. The environment should automatically activate (close and
+# open the powershell terminal in VS Code to refresh it). To confirm
+# it is active, look for `3.11.4 ('.venv': venv)` in the bottom right
+# corner of the VS Code screen. If it does not activate automatically,
+# type Ctrl-Shift-P and run
+#
+#     Python: Select Interpreter
+#     < Pick the Python version from the venv >
+#
+# When moving a virtual environment between two computers, consider
+# compressing it; the compression ratio is very good (e.g. 500 MiB to
+# 100 MiB).
+#
 # The packages in this virtual environment were installed using:
 #
 #   pip install numpy matplotlib pandas scikit-learn scikit-survival
