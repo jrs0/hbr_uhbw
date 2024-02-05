ac8b87b70bfcbd7af27890dd815a2bf90390b5f0
John Scott <john.scott@uhbw.nhs.uk>
2024-02-05 14:02:34 +0000

    Added documentation

--- pyhbr/src/pyhbr/common.py
+++ pyhbr/src/pyhbr/common.py
@@ -13,12 +13,16 @@ def read_sql(query, con_string = "mssql+pyodbc://dsn"):
     default connection string argument, set up a data source
     name called "dsn" using the program "ODBC Data Sources."
 
+    Any database server supported by sqlalchemy can be used
+    with this function. Specify the con_string for the
+    connection as described [here](https://docs.sqlalchemy.org/en/20/core/engines.html#database-urls).
+
     Args:
         query (str): The SQL query to execute
-        con_string (str)): sqlalchemy connection string
+        con_string (str): sqlalchemy connection string
 
     Returns:
-        pandas dataframe: The result of the query (table)
+        (pandas.DataFrame): The result of the query
     """
     con = sql.create_engine(con_string)
     result = pd.read_sql(query, con)
