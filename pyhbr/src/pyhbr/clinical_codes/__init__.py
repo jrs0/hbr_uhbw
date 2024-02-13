9eee594662c71ecb821de02e1be3a3c49763157b
John Scott <john.scott17@nhs.net>
2024-02-13 10:54:57 +0000

    Fixed the serde by removing @serde decorator (simplest way for now)

--- pyhbr/src/pyhbr/clinical_codes/__init__.py
+++ pyhbr/src/pyhbr/clinical_codes/__init__.py
@@ -1,25 +1,39 @@
+"""Contains utilities for clinical code groups
+"""
+
+from __future__ import annotations
 from importlib.resources import files
-from serde.yaml import from_yaml
 
 from dataclasses import dataclass
 from serde import serde
+from serde.yaml import from_yaml
 
-@serde
-@dataclass
-class Index:
-    """Index used to sort the code categories
-    """
-    index: str | tuple[str, str]
-
-@serde
+# Note: this class is missing the @serde decorator 
+# deliberately. It seems like there is an issue with
+# the class being recursively defined and the use of
+# the decorator. However, pyserde supports runtime
+# serde of classes without the @serde annotation 
+# (see https://github.com/yukinarit/pyserde/blob/
+# main/docs/en/decorators.md#deserializing-class
+# -without-serde), which works fine.
 @dataclass
 class Category:
     """Code/categories struct
+    
+    Attributes:
+        name: The name of the category (e.g. I20) or clinical code (I20.1)
+        docs: The description of the category or code
+        index: Used to sort a list of Categories
+        categories: For a category, the list of sub-categories contained.
+            None for a code.
+        exclude: Contains code groups which do not contain any members
+            from this category or any of its sub-categories. 
+        
     """
     name: str
     docs: str
-    index: Index
-    categories: list["Category"] | None
+    index: str | tuple[str, str]
+    categories: list[Category] | None
     exclude: set[str] | None
 
 @serde
@@ -45,6 +59,6 @@ def load_from_package(name: str) -> ClinicalCodeTree:
     """
     contents = files("pyhbr.clinical_codes.files").joinpath(name).read_text()
     return from_yaml(ClinicalCodeTree, contents)
-    #return yaml.load(contents, Loader=yaml.CLoader)
+    return yaml.load(contents, Loader=yaml.CLoader)
 
 
