9949bf96084f07231ec5d2f0bbc84777c74e803b
John Scott <john.scott@uhbw.nhs.uk>
2024-02-16 14:46:21 +0000

    Checked the index_episodes function runs

--- prototypes/hic_test.py
+++ prototypes/hic_test.py
@@ -6,6 +6,7 @@ import datetime as dt
 from pyhbr.common import make_engine, get_data
 from pyhbr.middle import from_hic
 from pyhbr.data_source import hic
+from pyhbr.analysis import arc_hbr
 
 import pandas as pd
 
@@ -22,54 +23,4 @@ prescriptions = from_hic.get_prescriptions(engine)
 demographics = get_data(engine, hic.demographics_query)
 episodes = get_data(engine, hic.episodes_query, start_date, end_date)
 
-def index_episodes(episodes: pd.DataFrame, codes: pd.DataFrame) -> pd.DataFrame:
-    """Get the index episodes for ACS/PCI patients
-
-    Index episodes are defined by the contents of the first episode of
-    the spell (i.e. the cause of admission to hospital). Episodes are
-    considered an index event if:
-
-    * The primary diagnosis contains an ACS ICD-10 code; or
-    * There is a PCI procedure in any primary or secondary position
-
-    The table returned contains only the episodes that match, along
-    with all the information about that episode present in the episodes
-    and codes tables.   
-
-    Args:
-        episodes: All patient episodes. Must contain `episode_id`, `spell_id`
-            and `episode_start`.
-        codes: All diagnosis/procedure codes by episode. Must contain
-            `episode_id`, `position` (indexed from 1 which is the primary 
-            code, >1 are secondary codes), and `group` (containing either `acs` 
-            or `pci`).
-
-    Returns:
-        The index episodes.
-    """
-
-    # Index episodes are defined by the contents of the first episode in the
-    # spell (to capture to cause of admission to hospital).
-    first_episodes = (
-        episodes.sort_values("episode_start").groupby("spell_id").head(1)
-    )
-
-    # Join the diagnosis/procedure codes (inner join reduces to episodes which
-    # have codes in any group, which is a superset of the index episodes)
-    first_episodes_with_codes = (
-        pd.merge(first_episodes, codes, how="inner", on="episode_id")
-    )
-
-    # ACS matches based on a primary diagnosis of ACS (this is to rule out
-    # cases where patient history may contain ACS recorded as a secondary
-    # diagnosis).
-    acs_match = (
-        (first_episodes_with_codes["group"] == "acs_bezin") &
-        (first_episodes_with_codes["position"] == 1)
-    )
-
-    # A PCI match is allowed anywhere in the procedures list, but must still
-    # be present in the index episode
-    pci_match = (first_episodes_with_codes["group"] == "pci")
-
-    return first_episodes_with_codes[acs_match | pci_match]
+index_episodes = arc_hbr.index_episodes(episodes, codes)
--- pyhbr/src/pyhbr/analysis/arc_hbr.py
+++ pyhbr/src/pyhbr/analysis/arc_hbr.py
@@ -1,3 +1,66 @@
 """Calculation of the ARC HBR score
 """
 
+import pandas as pd
+
+def index_episodes(episodes: pd.DataFrame, codes: pd.DataFrame) -> pd.DataFrame:
+    """Get the index episodes for ACS/PCI patients
+
+    Index episodes are defined by the contents of the first episode of
+    the spell (i.e. the cause of admission to hospital). Episodes are
+    considered an index event if:
+
+    * The primary diagnosis contains an ACS ICD-10 code; or
+    * There is a PCI procedure in any primary or secondary position
+
+    A prerequisite for an episode to be an index episode is that it
+    is present in both the episodes and codes table. The episodes table
+    contains start-time/spell information, and the codes table contains
+    information about what diagnoses/procedures occurred in the episode.
+
+    The table returned contains only the episodes that match, along
+    with all the information about that episode present in the episodes
+    and codes tables.   
+
+    If you need to modify this function, note that the group names used to
+    identify ACS/PCI groups are called `acs` and `pci` (present in the
+    codes table). Also note that the position column in the codes table
+    is indexed from 1.
+
+    Args:
+        episodes: All patient episodes. Must contain `episode_id`, `spell_id`
+            and `episode_start`.
+        codes: All diagnosis/procedure codes by episode. Must contain
+            `episode_id`, `position` (indexed from 1 which is the primary 
+            code, >1 are secondary codes), and `group` (containing either `acs` 
+            or `pci`).
+
+    Returns:
+        The index episodes.
+    """
+
+    # Index episodes are defined by the contents of the first episode in the
+    # spell (to capture to cause of admission to hospital).
+    first_episodes = (
+        episodes.sort_values("episode_start").groupby("spell_id").head(1)
+    )
+
+    # Join the diagnosis/procedure codes (inner join reduces to episodes which
+    # have codes in any group, which is a superset of the index episodes)
+    first_episodes_with_codes = (
+        pd.merge(first_episodes, codes, how="inner", on="episode_id")
+    )
+
+    # ACS matches based on a primary diagnosis of ACS (this is to rule out
+    # cases where patient history may contain ACS recorded as a secondary
+    # diagnosis).
+    acs_match = (
+        (first_episodes_with_codes["group"] == "acs_bezin") &
+        (first_episodes_with_codes["position"] == 1)
+    )
+
+    # A PCI match is allowed anywhere in the procedures list, but must still
+    # be present in the index episode
+    pci_match = (first_episodes_with_codes["group"] == "pci")
+
+    return first_episodes_with_codes[acs_match | pci_match]
