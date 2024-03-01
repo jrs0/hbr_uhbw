40329d61eeca108b495510ada136a4ad74f0eaff
John Scott <john.scott17@nhs.net>
2024-03-01 12:44:52 +0000

    Added some simple window config

--- pyhbr/src/pyhbr/clinical_codes/codes_editor/codes_editor.py
+++ pyhbr/src/pyhbr/clinical_codes/codes_editor/codes_editor.py
@@ -1,7 +1,23 @@
-from PyQt6.QtWidgets import QApplication, QWidget
+from PyQt6.QtWidgets import QApplication, QWidget, QMainWindow, QPushButton
 
 # Only needed for access to command line arguments
 import sys
+
+# Subclass QMainWindow to customize your application's main window
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("PyHBR Clinical Code Group Editor")
+        button = QPushButton("Press Me!")
+
+        # Set the central widget of the Window.
+        self.setCentralWidget(button)
+        button.setCheckable(True)
+        button.clicked.connect(self.the_button_was_clicked)
+
+    def the_button_was_clicked(self):
+        print("Clicked!")
 
 def run_app() -> None:
     """Run the main codes editor application
@@ -13,8 +29,7 @@ def run_app() -> None:
     app = QApplication(sys.argv)
 
     # Create a Qt widget, which will be our window.
-    window = QWidget()
-    window.setWindowTitle("PyHBR Clinical Code Group Editor")
+    window = MainWindow()
     window.show()
 
     # Start the event loop.
