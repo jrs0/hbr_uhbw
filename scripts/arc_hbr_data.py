865020e0a866702384e9e37813d2d3edc21de53e
John Scott <john.scott@uhbw.nhs.uk>
2024-03-13 13:44:08 +0000

    Moved model to its own script
    Will come back to modelling later,
    more data processing to do first

--- scripts/arc_hbr_data.py
+++ scripts/arc_hbr_data.py
@@ -74,35 +74,3 @@ data = {
 }
 
 common.save_item(data, "arc_hbr_data")
-
-# ======= QUICK MODEL =======
-
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import train_test_split
-from numpy.random import RandomState
-from sklearn.metrics import roc_auc_score
-
-random_state = RandomState(0)
-
-# Granular features
-X1 = features.drop(columns=["gender"]).dropna() # To avoid one-hot encoding for now
-
-# ARC Score
-X2 = arc_hbr_score.loc[X1.index]
-
-# Combine
-X = pd.concat([X1, X2], axis=1)
-
-# Binary bleeding outcome
-y = bleeding_outcome.loc[X.index] > 0
-
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state=random_state)
-
-
-model = RandomForestClassifier()
-
-fit = model.fit(X_train, y_train)
-
-probs = fit.predict_proba(X_test)
-
-auc = roc_auc_score(y_test, probs[:,1])
