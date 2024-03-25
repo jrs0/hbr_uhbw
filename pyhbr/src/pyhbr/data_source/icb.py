1d47e920b4f103f6fbbed62934e47259aba73f91
John Scott <john.scott17@nhs.net>
2024-03-25 13:15:46 +0000

    Added code to fetch the diagnosis/procedure columns from ICB HES

---
+++ pyhbr/src/pyhbr/data_source/icb.py
@@ -0,0 +1,102 @@
+"""Data sources available from the BNSSG ICB
+
+This file contains queries that fetch the raw data from the BNSSG
+ICB, which includes hospital episode statistics (HES) and primary
+care data.
+
+This file does not include the HIC data transferred to the ICB.
+"""
+
+from itertools import product
+from datetime import date
+from sqlalchemy import select, Select, Engine, String
+from pyhbr.common import CheckedTable
+
+
+def ordinal(n: int) -> str:
+    """Make an an ordinal like "2nd" from a number n
+
+    See https://stackoverflow.com/a/20007730.
+
+    Args:
+        n: The integer to convert to an ordinal string.
+
+    Returns:
+        For an integer (e.g. 5), the ordinal string (e.g. "5th")
+    """
+    if 11 <= (n % 100) <= 13:
+        suffix = "th"
+    else:
+        suffix = ["th", "st", "nd", "rd", "th"][min(n % 10, 4)]
+    return str(n) + suffix
+
+
+def clinical_code_column_name(kind: str, position: int) -> str:
+    """_summary_
+
+    Args:
+        kind: Either "diagnosis" or "procedure".
+        position: 0 for primary, 1 and higher for secondaries.
+
+    Returns:
+        The column name for the clinical code compatible with
+            the ICB HES tables.
+    """
+
+    if kind == "diagnosis":
+        if position == 0:
+            return "DiagnosisPrimary_ICD"
+
+        return f"Diagnosis{ordinal(position)}Secondary_ICD"
+    else:
+        if position == 0:
+            # reversed compared to diagnoses
+            return "PrimaryProcedure_OPCS"
+
+        # Secondaries are offset by one compared to diagnoses
+        return f"Procedure{ordinal(position+1)}_OPCS"
+
+
+def episodes_query(engine: Engine, start_date: date, end_date: date) -> Select:
+    """Get the episodes list in the HES data
+
+    This table contains
+
+    Args:
+        engine: the connection to the database
+        start_date: first valid consultant-episode start date
+        end_date: last valid consultant-episode start date
+
+    Returns:
+        SQL query to retrieve episodes table
+    """
+    table = CheckedTable("tbl_apc_sem_all", engine)
+
+    # Standard columns containing IDs, dates, patient demographics, etc
+    columns = [
+        table.col("AIMTC_Pseudo_NHS").cast(String).label("patient_id"),
+        table.col("PBRspellID").cast(String).label("spell_id"),
+        table.col("StartDate_ConsultantEpisode").label("episode_start"),
+        table.col("EndDate_ConsultantEpisode").label("episode_end"),
+    ]
+
+    # Diagnosis and procedure columns are renamed to (diagnosis|procedure)_n,
+    # where n begins from 1 (for the primary code; secondaries are represented
+    # using n > 1)
+    clinical_code_column_names = {
+        clinical_code_column_name(kind, n): f"{kind}_{n+1}"
+        for kind, n in product(["diagnosis", "procedure"], range(24))
+    }
+
+    clinical_code_columns = [
+        table.col(real_name).cast(String).label(new_name)
+        for real_name, new_name in clinical_code_column_names.items()
+    ]
+    
+    # Append the clinical code columns to the other data columns
+    columns += clinical_code_columns
+
+    return select(*columns).where(
+        table.col("StartDate_ConsultantEpisode") >= start_date,
+        table.col("EndDate_ConsultantEpisode") <= end_date,
+    )
---
+++ scripts/icb_data.py
@@ -0,0 +1,81 @@
+"""Calculate the ARC HBR score for the HIC data
+"""
+
+import datetime as dt
+import matplotlib.pyplot as plt
+import pandas as pd
+
+from pyhbr import common
+from pyhbr.data_source import icb
+
+import importlib
+importlib.reload(common)
+importlib.reload(icb)
+
+pd.set_option("display.max_rows", 100)
+
+# Set a date range for episode fetch
+start_date = dt.date(2023, 1, 1)
+end_date = dt.date(2023, 2, 1)
+
+# Fetch data
+engine = common.make_engine(database="abi")
+data = common.get_data(engine, icb.episodes_query, start_date, end_date)
+
+# Get the index episodes (primary ACS or PCI anywhere in first episode)
+index_episodes = acs.index_episodes(hic_data)
+
+# Get other episodes relative to the index episode (for counting code
+# groups before/after the index)
+all_other_codes = counting.get_all_other_codes(index_episodes, hic_data)
+
+# Get the episodes that occurred in the previous year (for clinical code features)
+max_before = dt.timedelta(days=365)
+min_before = dt.timedelta(days=30)
+previous_year = counting.get_time_window(all_other_codes, -max_before, -min_before)
+
+# Get the episodes that occurred in the following year (for clinical code outcomes)
+min_after = dt.timedelta(hours=72)  # Exclude periprocedural events
+max_after = dt.timedelta(days=365)
+following_year = counting.get_time_window(all_other_codes, min_after, max_after)
+
+# Calculate more granular features as an intermediate step for calculating the
+# ARC HBR score. Choose a function to extract the a value for the laboratory
+# measurements at index.
+features = arc_hbr.get_features(
+    index_episodes, previous_year, hic_data, arc_hbr.first_index_spell_result
+)
+
+# Calculate the ARC HBR score from the more granular features.
+arc_hbr_score = arc_hbr.get_arc_hbr_score(features, hic_data)
+
+# Get the bleeding outcome
+bleeding_groups = ["bleeding_al_ani"]
+bleeding_outcome = counting.count_code_groups(
+    index_episodes, following_year, bleeding_groups, True
+)
+
+# Get the bleed episodes (for chart review) -- bleeding in any position
+groups = following_year[following_year["group"].isin(bleeding_groups)].groupby("base_episode_id")
+for key, _  in groups:
+    print(groups.get_group(key), "\n\n")
+
+arc_hbr.plot_index_measurement_distribution(features)
+plt.show()
+
+arc_hbr.plot_arc_score_distribution(arc_hbr_score)
+plt.show()
+
+arc_hbr.plot_prescriptions_distribution(hic_data)
+plt.show()
+
+# Package the data up for saving
+data = {
+    "hic_data": hic_data,
+    "index_episodes": index_episodes,
+    "features": features,
+    "arc_hbr_score": arc_hbr_score,
+    "bleeding_outcome": bleeding_outcome,
+}
+
+common.save_item(data, "arc_hbr_data")
