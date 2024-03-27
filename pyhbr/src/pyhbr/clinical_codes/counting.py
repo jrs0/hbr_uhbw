b65ae3d274de2b754e6a1362bdc11e6540dbdc69
John Scott <john.scott17@nhs.net>
2024-03-27 10:57:41 +0000

    Taken another pass over code counting function

--- pyhbr/src/pyhbr/clinical_codes/counting.py
+++ pyhbr/src/pyhbr/clinical_codes/counting.py
@@ -3,54 +3,71 @@
 
 from pandas import DataFrame, Series
 from datetime import timedelta
-from pyhbr.middle.from_hic import HicData
 
 
-def get_all_other_codes(base_episodes: DataFrame, data: HicData) -> DataFrame:
-    """For each patient, get clinical codes in other episodes before/after the base
+def get_all_other_codes(
+    index_spells: DataFrame, data: dict[str, DataFrame]
+) -> DataFrame:
+    """For each patient, get clinical codes in other episodes before/after the index
 
-    This makes a table of base episodes along with all other episodes for a patient.
-    Two columns `base_episode_id` and `other_episode_id` identify the two episodes
-    for each row (they may be equal), and other information is stored such as the
-    time of the base episode, the time to the other episode, and clinical code information
-    for the other episode.
+    This makes a table of index episodes (which is the first episode of the index spell)
+    along with all other episodes for a patient. Two columns `index_episode_id` and
+    `other_episode_id` identify the two episodes for each row (they may be equal), and
+    other information is stored such as the time of the base episode, the time to the
+    other episode, and clinical code information for the other episode.
 
     This table is used as the basis for all processing involving counting codes before
     and after an episode.
 
+    !!! note
+        Episodes will not be included in the result if they do not have any clinical
+            codes that are in any code group.
+
     Args:
-        base_episodes: Contains `episode_id` as an index.
-        data: A class containing two DataFrame attributes:
-            * episodes: Contains `episode_id` as an index, and `patient_id` and `episode_start` as columns
-            * codes: Contains `episode_id` and other code data as columns
+        index_spells: Contains `episode_id` as an index.
+        data: A dictionary containing two DataFrame values:
+            * "episodes": Contains `episode_id` as an index, and `patient_id` and `episode_start` as columns
+            * "codes": Contains `episode_id` and other code data as columns
 
     Returns:
-        A table containing columns `base_episode_id`, `other_episode_id`,
-            `base_episode_start`, `time_to_other_episode`, and code data columns
+        A table containing columns `index_episode_id`, `other_episode_id`,
+            `index_episode_start`, `time_to_other_episode`, and code data columns
             for the other episode. Note that the base episode itself is included
             as an other episode.
     """
 
     # Remove everything but the index episode_id (in case base_episodes
     # already has the columns)
-    df = base_episodes[[]]
+    df = index_spells.reset_index(names="spell_id").set_index("episode_id")[
+        ["spell_id"]
+    ]
 
-    base_episode_info = df.merge(
-        data.episodes[["patient_id", "episode_start"]], how="left", on="episode_id"
-    ).rename(columns={"episode_start": "base_episode_start"})
+    index_episode_info = df.merge(
+        data["episodes"][["patient_id", "episode_start"]], how="left", on="episode_id"
+    ).rename(
+        columns={"episode_start": "index_episode_start", "spell_id": "index_spell_id"}
+    )
 
-    other_episodes = base_episode_info.reset_index(names="base_episode_id").merge(
-        data.episodes[["episode_start", "patient_id"]].reset_index(names="other_episode_id"),
-        how="left",
-        on="patient_id",
+    other_episodes = (
+        index_episode_info.reset_index(names="index_episode_id")
+        .merge(
+            data["episodes"][["episode_start", "patient_id", "spell_id"]].reset_index(
+                names="other_episode_id"
+            ),
+            how="left",
+            on="patient_id",
+        )
+        .rename(columns={"spell_id": "other_spell_id"})
     )
 
     other_episodes["time_to_other_episode"] = (
-        other_episodes["episode_start"] - other_episodes["base_episode_start"]
+        other_episodes["episode_start"] - other_episodes["index_episode_start"]
     )
 
+    # Use an inner join to filter out other episodes that have no associated codes
+    # in any group.
     with_codes = other_episodes.merge(
-        data.codes, how="left", left_on="other_episode_id", right_on="episode_id"
+        data["codes"], how="inner", left_on="other_episode_id", right_on="episode_id"
     ).drop(columns=["patient_id", "episode_start", "episode_id"])
 
     return with_codes
@@ -96,23 +113,25 @@ def get_time_window(
 
 
 def count_code_groups(
-    index_episodes: DataFrame,
+    index_spells: DataFrame,
     other_episodes: DataFrame,
     code_groups: list[str],
     any_position: bool,
+    exclude_index_spell: bool,
+    first_episode_only: bool,
 ) -> Series:
-    """Count occurrences of prior code group in the previous year
+    """Count occurrences of a code group in a set of other episodes
 
-    Count the total occurrences of the codes in any of code_groups in
-    the other episodes before the index episode specified in
-    previous_year.
+    Count the total occurrences of the codes in any of a list of
+    code groups in the other episodes relative to the index spells.
 
     Note that the index episode itself will not necessarily be
     excluded from the count (if other_episodes contains the index
-    episode).
+    episode, and exclude_index_spell is False).
 
     Args:
-        index_episodes: Any DataFrame with `episode_id` as index
+        index_episodes: Any DataFrame with `spell_id` as Pandas index and
+            containing an `episode_id` column.
         other_episodes: Table of other episodes (relative to the index).
             This can be narrowed to either the previous or subsequent
             year, or a different time frame. (In particular, exclude the
@@ -122,31 +141,49 @@ def count_code_groups(
         any_position: If True, count any code in any diagnosis/procedure
             position. If False, only count a code if it is the primary
             diagnosis/procedure.
-
-    TODO: add another argument for first_episode (bool), to narrow to
-    first episode of spell. Needs spell_id in the all_other_codes table.
-
-    BUG: This function probably double-counts codes if they appear in
-    two groups. Can be fixed by dropping code duplicates after narrowing
-    by group, and then counting the result of that. Needs some testing.
+        exclude_index_spell: Do not include any code in the count if it
+            is from an episode in the index spell.
+        first_episode_only: Only count codes if they come from the first
+            episode of a spell.
 
     Returns:
         A series containing the number of code group occurrences in the
             other_episodes table.
     """
 
-    code_group_count = (
-        other_episodes[
-            (any_position | (other_episodes["position"] == 1))
-            & (other_episodes["group"].isin(code_groups))
-        ]
-        .groupby("base_episode_id")
-        .size()
-        .rename("code_group_count")
-    )
+    # Reduce to only the code groups of interest
+    df = other_episodes
+    df = df[df["group"].isin(code_groups)]
+
+    # Duplicated rows correspond to codes in multiple
+    # code groups. A code is duplicated if it has the
+    # same type (diagnosis/procedure) and position as
+    # another code in the same other episode.
+    df = df.drop_duplicates(["other_episode_id", "type", "position"])
+
+    # Keep only necessary columns
+    df = df[["index_spell_id", "other_spell_id", "position", "time_to_other_episode"]]
+
+    # Optionally remove rows corresponding to the index spell
+    if exclude_index_spell:
+        df = df[~(df["other_spell_id"] == df["index_spell_id"])]
+
+    # Optionally retain only the first episode in each other spell.
+    # BUG: there is an issue here if the other_episode_start has only
+    # day granularity, and the first and second episodes begin on the
+    # same day.
+    if first_episode_only:
+        df = df.groupby("other_spell_id").sort_values("time_to_other_episode").head(1)
+
+    # Optionally exclude secondary diagnoses/procedures
+    if not any_position:
+        df = df[df["position"] == 1]
+
+    # Rows now correspond to codes that can be counted relative to the index spell
+    code_group_count = df.groupby("index_spell_id").size().rename("code_group_count")
 
     return (
-        index_episodes[[]]
+        index_spells[[]]
         .merge(code_group_count, how="left", left_index=True, right_index=True)
         .fillna(0.0)["code_group_count"]
     )
--- scripts/icb_basic_preprocess.py
+++ scripts/icb_basic_preprocess.py
@@ -1,10 +1,25 @@
 """Convert the ICB data to ACS/bleeding/ischaemia datasets
 """
 
+import importlib
+import datetime as dt
 from pyhbr import common
+from pyhbr.clinical_codes import counting
+
+importlib.reload(counting)
 
 data = common.load_item("icb_basic_data")
 
-common.save_item(data, "icb_basic_data")
+index_spells = data["index_spells"]
 
-data["primary_care_prescriptions"]
+# Get other episodes relative to the index episode (for counting code
+# groups before/after the index).
+all_other_codes = counting.get_all_other_codes(index_spells, data)
+
+# Get the episodes that occurred in the previous year (for clinical code features)
+max_before = dt.timedelta(days=365)
+min_before = dt.timedelta(days=30)
+previous_year = counting.get_time_window(all_other_codes, -max_before, -min_before)
+
+
+counting.count_code_groups(index_spells, previous_year, ["bleeding_adaptt"], False)
