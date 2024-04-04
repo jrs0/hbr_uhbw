3b86dbd1ce85e253af1afa484e4ad6adce712e62
John Scott <john.scott17@nhs.net>
2024-04-04 14:57:43 +0100

    Compiled all the columns into one training dataset

--- pyhbr/src/pyhbr/middle/from_icb.py
+++ pyhbr/src/pyhbr/middle/from_icb.py
@@ -1,9 +1,12 @@
 import numpy as np
+import pandas as pd
 from pandas import DataFrame, Series, concat
 from pyhbr import clinical_codes, common
 from pyhbr.data_source import icb
 from sqlalchemy import Engine
 from datetime import date
+import datetime as dt
+from pyhbr.clinical_codes import counting
 
 
 def get_episodes(raw_sus_data: DataFrame) -> DataFrame:
@@ -578,3 +581,110 @@ def preprocess_ethnicity(column: Series) -> Series:
     return column.map(ethnicity_map).astype("category")
 
 
+def blood_pressure(
+    swd_index_spells: DataFrame, primary_care_measurements: DataFrame
+) -> DataFrame:
+    """Get recent blood pressure readings
+
+    Args:
+        primary_care_measurements: Contains a `name` column containing
+            the measurement name (expected to contain "blood_pressure"),
+            a `result` column with the format "systolic/diastolic" for
+            the blood pressure rows, a `date`, and a `patient_id`.
+        swd_index_spells: Has Pandas index `spell_id`, and columns
+            `patient_id` and `spell_start`.
+
+    Returns:
+        A dataframe index by `spell_id` containing `bp_systolic`
+            and `bp_diastolic` columns.
+    """
+
+    df = primary_care_measurements
+
+    # Drop rows where the measurement is not known
+    df = df[~df["name"].isna()]
+
+    # Drop rows where the prescription date is not known
+    df = df[~df["date"].isna()]
+
+    blood_pressure = df[df.name.str.contains("blood_pressure")][
+        ["patient_id", "date", "result"]
+    ].copy()
+    blood_pressure[["bp_systolic", "bp_diastolic"]] = (
+        df["result"].str.split("/", expand=True).apply(pd.to_numeric, errors="coerce")
+    )
+
+    # Join the prescriptions to the index spells
+    df = (
+        swd_index_spells[["spell_start", "patient_id"]]
+        .reset_index()
+        .merge(blood_pressure, how="left", on="patient_id")
+    )
+    df["time_to_index_spell"] = df["spell_start"] - df["date"]
+
+    # Only keep measurements occurring in the year before the index event
+    min_before = dt.timedelta(days=0)
+    max_before = dt.timedelta(days=60)
+    bp_before_index = counting.get_time_window(
+        df, -max_before, -min_before, "time_to_index_spell"
+    )
+
+    most_recent_bp = bp_before_index.sort_values("date").groupby("spell_id").tail(1)
+    prior_bp = swd_index_spells.merge(
+        most_recent_bp, how="left", on="spell_id"
+    ).set_index("spell_id")[["bp_systolic", "bp_diastolic"]]
+
+    return prior_bp
+
+def hba1c(
+    swd_index_spells: DataFrame, primary_care_measurements: DataFrame
+) -> DataFrame:
+    """Get recent HbA1c from the primary care measurements
+
+    Args:
+        primary_care_measurements: Contains a `name` column containing
+            the measurement name (expected to contain "blood_pressure"),
+            a `result` column with the format "systolic/diastolic" for
+            the blood pressure rows, a `date`, and a `patient_id`.
+        swd_index_spells: Has Pandas index `spell_id`, and columns
+            `patient_id` and `spell_start`.
+
+    Returns:
+        A dataframe indexed by `spell_id` containing recent (within 2 months)
+            HbA1c values.
+    """
+
+    df = primary_care_measurements
+
+    # Drop rows where the measurement is not known
+    df = df[~df["name"].isna()]
+
+    # Drop rows where the prescription date is not known
+    df = df[~df["date"].isna()]
+
+    hba1c = df[df.name.str.contains("hba1c")][
+        ["patient_id", "date", "result"]
+    ].copy()
+    hba1c["hba1c"] = pd.to_numeric(hba1c["result"], errors="coerce")
+
+    # Join the prescriptions to the index spells
+    df = (
+        swd_index_spells[["spell_start", "patient_id"]]
+        .reset_index()
+        .merge(hba1c, how="left", on="patient_id")
+    )
+    df["time_to_index_spell"] = df["spell_start"] - df["date"]
+
+    # Only keep measurements occurring in the year before the index event
+    min_before = dt.timedelta(days=0)
+    max_before = dt.timedelta(days=60)
+    hba1c_before_index = counting.get_time_window(
+        df, -max_before, -min_before, "time_to_index_spell"
+    )
+
+    most_recent_hba1c = hba1c_before_index.sort_values("date").groupby("spell_id").tail(1)
+    prior_hba1c = swd_index_spells.merge(
+        most_recent_hba1c, how="left", on="spell_id"
+    ).set_index("spell_id")[["hba1c"]]
+
+    return prior_hba1c
--- scripts/icb_basic_preprocess.py
+++ scripts/icb_basic_preprocess.py
@@ -6,7 +6,6 @@ set.
 """
 
 import importlib
-import datetime as dt
 import numpy as np
 from pyhbr import common
 from pyhbr.clinical_codes import counting
@@ -70,119 +69,10 @@ prior_prescriptions = acs.prescriptions_before_index(
 
 primary_care_measurements = data["primary_care_measurements"]
 
-
-def blood_pressure(
-    swd_index_spells: DataFrame, primary_care_measurements: DataFrame
-) -> DataFrame:
-    """Get recent blood pressure readings
-
-    Args:
-        primary_care_measurements: Contains a `name` column containing
-            the measurement name (expected to contain "blood_pressure"),
-            a `result` column with the format "systolic/diastolic" for
-            the blood pressure rows, a `date`, and a `patient_id`.
-        swd_index_spells: Has Pandas index `spell_id`, and columns
-            `patient_id` and `spell_start`.
-
-    Returns:
-        A dataframe index by `spell_id` containing `bp_systolic`
-            and `bp_diastolic` columns.
-    """
-
-    df = primary_care_measurements
-
-    # Drop rows where the measurement is not known
-    df = df[~df["name"].isna()]
-
-    # Drop rows where the prescription date is not known
-    df = df[~df["date"].isna()]
-
-    blood_pressure = df[df.name.str.contains("blood_pressure")][
-        ["patient_id", "date", "result"]
-    ].copy()
-    blood_pressure[["bp_systolic", "bp_diastolic"]] = (
-        df["result"].str.split("/", expand=True).apply(pd.to_numeric, errors="coerce")
-    )
-
-    # Join the prescriptions to the index spells
-    df = (
-        swd_index_spells[["spell_start", "patient_id"]]
-        .reset_index()
-        .merge(blood_pressure, how="left", on="patient_id")
-    )
-    df["time_to_index_spell"] = df["spell_start"] - df["date"]
-
-    # Only keep measurements occurring in the year before the index event
-    min_before = dt.timedelta(days=0)
-    max_before = dt.timedelta(days=60)
-    bp_before_index = counting.get_time_window(
-        df, -max_before, -min_before, "time_to_index_spell"
-    )
-
-    most_recent_bp = bp_before_index.sort_values("date").groupby("spell_id").tail(1)
-    prior_bp = swd_index_spells.merge(
-        most_recent_bp, how="left", on="spell_id"
-    ).set_index("spell_id")[["bp_systolic", "bp_diastolic"]]
-
-    return prior_bp
-
-
-prior_blood_pressure = blood_pressure(swd_index_spells, primary_care_measurements)
-
-def hba1c(
-    swd_index_spells: DataFrame, primary_care_measurements: DataFrame
-) -> DataFrame:
-    """Get recent HbA1c from the primary care measurements
-
-    Args:
-        primary_care_measurements: Contains a `name` column containing
-            the measurement name (expected to contain "blood_pressure"),
-            a `result` column with the format "systolic/diastolic" for
-            the blood pressure rows, a `date`, and a `patient_id`.
-        swd_index_spells: Has Pandas index `spell_id`, and columns
-            `patient_id` and `spell_start`.
-
-    Returns:
-        A dataframe indexed by `spell_id` containing recent (within 2 months)
-            HbA1c values.
-    """
-
-    df = primary_care_measurements
-
-    # Drop rows where the measurement is not known
-    df = df[~df["name"].isna()]
-
-    # Drop rows where the prescription date is not known
-    df = df[~df["date"].isna()]
-
-    hba1c = df[df.name.str.contains("hba1c")][
-        ["patient_id", "date", "result"]
-    ].copy()
-    hba1c["hba1c"] = pd.to_numeric(hba1c["result"], errors="coerce")
-
-    # Join the prescriptions to the index spells
-    df = (
-        swd_index_spells[["spell_start", "patient_id"]]
-        .reset_index()
-        .merge(hba1c, how="left", on="patient_id")
-    )
-    df["time_to_index_spell"] = df["spell_start"] - df["date"]
-
-    # Only keep measurements occurring in the year before the index event
-    min_before = dt.timedelta(days=0)
-    max_before = dt.timedelta(days=60)
-    hba1c_before_index = counting.get_time_window(
-        df, -max_before, -min_before, "time_to_index_spell"
-    )
-
-    most_recent_hba1c = hba1c_before_index.sort_values("date").groupby("spell_id").tail(1)
-    prior_hba1c = swd_index_spells.merge(
-        most_recent_hba1c, how="left", on="spell_id"
-    ).set_index("spell_id")[["hba1c"]]
-
-    return prior_hba1c
-
-prior_hba1c = hba1c(swd_index_spells, primary_care_measurements)
+# Only blood pressure and HbA1c go back to 2019 in the data -- not
+# including the other measurements in order to keep the sample size up.
+prior_blood_pressure = from_icb.blood_pressure(swd_index_spells, primary_care_measurements)
+prior_hba1c = from_icb.hba1c(swd_index_spells, primary_care_measurements)
 
 # Combine all tables (features and outcomes) into a single table
 # for saving.
@@ -191,9 +81,13 @@ training_data = (
     .merge(outcomes, how="left", on="spell_id")
     .merge(code_features, how="left", on="spell_id")
     .merge(index_attributes, how="left", on="spell_id")
+    .merge(prior_prescriptions, how="left", on="spell_id")
+    .merge(prior_blood_pressure, how="left", on="spell_id")
+    .merge(prior_hba1c, how="left", on="spell_id")
 )
-
 
 rate_summaries = describe.get_column_rates(
     training_data.select_dtypes(include="number")
 )
+
+common.save_data(training_data, "icb_basic_training")
