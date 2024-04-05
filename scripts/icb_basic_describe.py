f5291a689134db718b38c5cf4616b4c6eac0d235
John Scott <john.scott17@nhs.net>
2024-04-05 11:00:23 +0100

    Started plotting attributes properties
    Going to go back and modify flag columns to bool.

--- scripts/icb_basic_describe.py
+++ scripts/icb_basic_describe.py
@@ -1,7 +1,33 @@
+import pandas as pd
 from pyhbr import common
 from pyhbr.analysis import describe
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 icb_basic_data = common.load_item("icb_basic_data")
 
-describe.proportion_missingness(icb_basic_data["features_attributes"])
+# Get datasets for convenience
+outcomes = icb_basic_data["outcomes"]
+features_attributes = icb - basic_data["features_attributes"]
 
+# Check outcomes
+missing_outcomes = describe.proportion_missingness(outcomes)
+print("The amount of missing data in the outcome columns is:")
+print(missing_outcomes)
+prevalence_outcomes = describe.get_column_rates(outcomes)
+print("The rate of occurrence of each outcome is:")
+print(prevalence_outcomes)
+
+# Check attributes
+missing_attributes = describe.proportion_missingness(features_attributes)
+print("The amount of missing data in the attributes columns is:")
+print(missing_attributes)
+
+
+numeric_attributes = features_attributes.select_dtypes(include="number").iloc[:,0:16].melt()
+sns.catplot(numeric_attributes, y="value", kind="box", col="variable", col_wrap=8, sharey=False)
+plt.show()
+
+
+#.boxplot(meanline=True, showmeans=True)
+#plt.show()
