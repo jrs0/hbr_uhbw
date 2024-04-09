8dd8eeedc0e916964c562698f77c1eca9d9a9f51
John Scott <john.scott17@nhs.net>
2024-04-09 15:50:17 +0100

    Got the preprocessing working for general columns

--- scripts/icb_basic_model.py
+++ scripts/icb_basic_model.py
@@ -46,31 +46,64 @@ X_train, X_test, y_train, y_test = train_test_split(
     features, binary_outcome, test_size=0.25, random_state=random_state
 )
 
-# To one-hot encode
-object_columns = features.columns[
+# Category columns should be one-hot encoded (in all these one-hot encoders,
+# consider the effect of linear dependence among the columns due to the extra
+# variable compared to dummy encoding -- the relevant parameter is called
+# 'drop').
+cat_columns = features.columns[
     (features.dtypes == "object") | (features.dtypes == "category")
 ]
-object_pipe = Pipeline([
-    #("low_variance", VarianceThreshold()),
-    ("one_hot_encode", OneHotEncoder(handle_unknown="infrequent_if_exist", min_frequency=0.01)),
-])
-one_hot_encoders = [
+cat_pipe = Pipeline(
+    [
+        (
+            "one_hot_encode",
+            OneHotEncoder(handle_unknown="infrequent_if_exist", min_frequency=0.002),
+        ),
+    ]
+)
+cat_preprocess = [
     (
-        "object_preprocess",
-        object_pipe,
-        object_columns,
+        "cat_preprocess",
+        cat_pipe,
+        cat_columns,
     )
 ]
 
-# To center and scale
-float_columns = features.columns[(features.dtypes == "float")]
-float_pipe = Pipeline([
-    ("low_variance", VarianceThreshold()),
-    ("impute", SimpleImputer(missing_values=np.nan, strategy='mean')),
-    ("scaler", StandardScaler())
-])
+# Flag columns (encoded using Int8, which supports NaN), should be one-hot
+# encoded (considered separately from category in case I want to do something
+# different with these).
+flag_columns = features.columns[(features.dtypes == "Int8")]
+flag_df = features.loc[:, flag_columns]  # Look at the columns
+flag_pipe = Pipeline(
+    [
+        (
+            "one_hot_encode",
+            OneHotEncoder(handle_unknown="infrequent_if_exist"),
+        ),
+    ]
+)
+flag_preprocess = [
+    (
+        "flag_preprocess",
+        flag_pipe,
+        flag_columns,
+    )
+]
 
-scalers = [
+
+# Numerical columns -- impute missing values, remove low variance
+# columns, and then centre and scale the rest.
+float_columns = features.columns[(features.dtypes == "float")]
+float_df = features.loc[:, float_columns]  # Look at the columns
+float_pipe = Pipeline(
+    [
+        ("impute", SimpleImputer(missing_values=np.nan, strategy="mean")),
+        ("low_variance", VarianceThreshold()),
+        ("scaler", StandardScaler()),
+    ]
+)
+
+float_preprocess = [
     (
         "float_preprocess",
         float_pipe,
@@ -79,16 +112,47 @@ scalers = [
 ]
 
 preprocess = ColumnTransformer(
-    one_hot_encoders + scalers,
+    cat_preprocess + float_preprocess + flag_preprocess,
     remainder="drop",
 )
 
-model = RandomForestClassifier()
+X_prep_fit = preprocess.fit(X_train)
+X_prep = X_prep_fit.transform(X_train)
+
+# Get the output feature names for the float columns. The only step that
+# can change columns is the low_variance reducer.
+float_output_features = X_prep_fit.named_transformers_["float_preprocess"][
+    "low_variance"
+].get_feature_names_out(float_columns)
+
+
+# Get the output feature names for category columns (one-hot encoded)
+cat_output_features = X_prep_fit.named_transformers_["cat_preprocess"][
+    "one_hot_encode"
+].get_feature_names_out()
+
+# Get the output feature names for the flag columns (one-hot encoded)
+flag_output_features = X_prep_fit.named_transformers_["flag_preprocess"][
+    "one_hot_encode"
+].get_feature_names_out()
+
+
+# This is the map showing which column ranges in the output features 
+# correspond to which column-transformer features lists. (map from
+# strings to slices)
+X_prep_fit.output_indices_
+
+outcome_name = "bleeding_outcome"
+
+model = RandomForestClassifier(
+    n_estimators=100, max_depth=10, random_state=random_state
+)
 
 pipe = Pipeline([("preprocess", preprocess), ("model", model)])
 
-fit = pipe.fit(X_train, y_train.iloc[:, 0])
+fit = pipe.fit(X_train, y_train.loc[:, outcome_name])
 
 probs = fit.predict_proba(X_test)
 
-auc = roc_auc_score(y_test.iloc[:, 0], probs[:, 1])
+auc = roc_auc_score(y_test.loc[:, outcome_name], probs[:, 1])
+auc
