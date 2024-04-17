4a1a13b25bf5aac4fb0c6ba81a8f25dab3875c98
John Scott <john.scott17@nhs.net>
2024-04-17 11:54:04 +0100

    Formatting

--- scripts/icb_basic_data.py
+++ scripts/icb_basic_data.py
@@ -168,7 +168,9 @@ features_attributes = acs.remove_features(
 all_other_codes = counting.get_all_other_codes(index_spells, icb_basic_tmp)
 
 # Get the bleeding and ischaemia outcomes
-outcomes = acs.get_outcomes(index_spells, all_other_codes, date_of_death, cause_of_death)
+outcomes = acs.get_outcomes(
+    index_spells, all_other_codes, date_of_death, cause_of_death
+)
 
 features_codes = acs.get_code_features(index_spells, all_other_codes)
 
