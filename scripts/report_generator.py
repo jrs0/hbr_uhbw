c8990387a2d454eaca9c14a33e809f53f00dea75
John Scott <john.scott17@nhs.net>
2024-05-28 13:45:50 +0100

    Created a config file for report properties

--- scripts/report_generator.py
+++ scripts/report_generator.py
@@ -4,73 +4,63 @@
 
 import shutil
 import subprocess
+import argparse
+import copy
 from pathlib import Path
 from jinja2 import Environment, FileSystemLoader
-import argparse
+import yaml
 
 # Provide the option to render the quarto
-parser = argparse.ArgumentParser("icb_basic_report.py")
+parser = argparse.ArgumentParser("report_generator")
+parser.add_argument("-f", "--config-file", required=True, help="Specify the config file describing the report")
 parser.add_argument("-r", "--render", help="Render the auto-generated quarto report", action="store_true")
 parser.add_argument("-c", "--clean", help="Remove the build directory for this report", action="store_true")
 args = parser.parse_args()
 
-# Set up the 
-environment = Environment(loader=FileSystemLoader("templates/"))
-template = environment.get_template("report.qmd")
+# Read the configuration file
+
+with open(args.config_file) as stream:
+    try:
+        config = yaml.safe_load(stream)
+    except yaml.YAMLError as exc:
+        print(f"Failed to load config file: {exc}")
+        exit(1)
+
+# Set up the Jinja2 templates
+environment = Environment(loader=FileSystemLoader(config["templates_folder"]))
+template = environment.get_template(config["report_template"])
 
 # All outputs will be created inside the build folder
 # relative to the current working directory
-build_dir = Path("build")
+build_dir = Path(config["build_directory"])
 build_dir.mkdir(parents=True, exist_ok=True)
 
 # Make a subdirectory for this report
-report_dir = build_dir / Path("icb_basic_report")
+report_dir = build_dir / Path(config["report_subfolder"])
 report_dir.mkdir(parents=True, exist_ok=True)
 
-# Optionally clean
+# Optionally clean the subfolder
 if args.clean:
     shutil.rmtree(report_dir)
 
 # Set the directory for storing images
-image_source_dir = Path("../figures")
+image_source_dir = Path(config["image_source_dir"])
 image_dest_dir = report_dir / Path("images")
 image_dest_dir.mkdir(parents=True, exist_ok=True)
 
-# List the models to be included
-models = {
-    "random_forest": {
-        "text": "random forest",
-        "title": "Random Forest",
-        "abbr": "RF",
-    },
-    "logistic_regression": {
-        "text": "logistic regression",
-        "title": "Logistic Regression",
-        "abbr": "LR"
-    },
-    "xgboost": {
-        "text": "XGBoost",
-        "title": "XGBoost",
-        "abbr": "XGB"
-    }    
-}
+# Copy the config and adjust to create Jinja2 variables
+variables = copy.deepcopy(config)
+variables["bib_file"] = "ref.bib"
 
 # Copy images to the build folder
-for name, model in models.items():
+for name, model in config["models"].items():
     roc_image = Path(f"roc_{name}.png")
     shutil.copy(image_source_dir / roc_image, image_dest_dir / roc_image)
-    model["roc_curves_image"] = Path("images") / roc_image
+    variables["roc_curves_image"] = Path("images") / roc_image
 
 # Copy static files to output folder
-shutil.copy("../risk_management_file/ref.bib", report_dir / Path("ref.bib"))
-
-variables = {
-    "title": "Bleeding/Ischaemia Risk Models",
-    "subtitle": "Trained using hospital episode statistics and primary care data",
-    "author": "John Scott",
-    "bib_file": "ref.bib",
-    "models": models
-}
+print(config["bib_file"])
+shutil.copy(config["bib_file"], report_dir / Path("ref.bib"))
 
 # Render the report template and write it to the build directory
 doc = template.render(variables)
