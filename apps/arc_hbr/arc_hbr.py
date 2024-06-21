ac5d399499825466a5f51ffaefbf6e89b8cc0d62
John Scott <john.scott@uhbw.nhs.uk>
2024-06-21 11:00:34 +0100

    Added some prototype ag grid code

--- apps/arc_hbr/arc_hbr.py
+++ apps/arc_hbr/arc_hbr.py
@@ -1,14 +1,31 @@
 import streamlit as st
 import pandas as pd
+from faker import Faker
+from st_aggrid import AgGrid, JsCode, GridOptionsBuilder
+import numpy as np
+from random import randint
 
-df = pd.DataFrame(
-    [
-       {"command": "st.selectbox", "rating": 4, "is_widget": True},
-       {"command": "st.balloons", "rating": 5, "is_widget": False},
-       {"command": "st.time_input", "rating": 3, "is_widget": True},
-   ]
-)
-edited_df = st.data_editor(df)
+def random_tnumber():
+    num = randint(0, 9999999)
+    return f"T{num:>07}"
 
-favorite_command = edited_df.loc[edited_df["rating"].idxmax()]["command"]
-st.markdown(f"Your favorite command is **{favorite_command}** 🎈")
+fake = Faker()
+data = {
+    "Name": [fake.name() for n in range(2)],
+    "T Number": [random_tnumber() for n in range(2)],
+    "ARC Score": [1, 0],
+    "Age": [int(x) for x in np.random.normal(70, 10, 2)],
+}
+arc_score = pd.DataFrame(data)
+
+grid_builder = GridOptionsBuilder.from_dataframe(arc_score)
+grid_builder.configure_column(field="Name", headerTooltip="Hover to show T Number", tooltipField="T Number")
+
+grid_options = grid_builder.build()
+
+# Configure other properties directly
+grid_options["tooltipShowDelay"] = 500
+
+print(grid_options)
+
+AgGrid(arc_score, grid_options, allow_unsafe_jscode=True)
