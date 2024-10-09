dcb4aeae1da38d718933b0962b0a90e754c33aa7
John Scott <john.scott@uhbw.nhs.uk>
2024-10-09 16:39:09 +0100

    Added to the frailty script

--- scripts/frailty_data.py
+++ scripts/frailty_data.py
@@ -1,13 +1,19 @@
-# Script 
+# Script
 
 from pyhbr import common
 import pandas as pd
+import matplotlib.pyplot as plt
 
-data, data_path = common.load_item("icb_hic_data", interactive=True, save_dir="report/save_data")
+data, data_path = common.load_item(
+    "icb_hic_data", interactive=True, save_dir="report/save_data"
+)
 
 features_index = data["features_index"]
 management = data["info_management"]
+score = data["info_index_scores"]["cambridge_score"]
+attributes = data["features_attributes"]
 
-df = features_index.merge(management, on="spell_id", how="left")
+df = features_index.merge(management, on="spell_id", how="left").merge(
+    score, on="spell_id", how="left"
+)
 
-stemi_nstemi = df[df["stemi_index"] | df["nstemi_index"]]
