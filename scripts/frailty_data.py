89b5f08c6cfcafc8c71ad9c32816089e60dd1f7b
John Scott <john.scott@uhbw.nhs.uk>
2024-10-09 11:44:31 +0100

    Filtered to stemi/nstemi in frailty script

--- scripts/frailty_data.py
+++ scripts/frailty_data.py
@@ -1,10 +1,13 @@
+# Script 
+
 from pyhbr import common
 import pandas as pd
 
 data, data_path = common.load_item("icb_hic_data", interactive=True, save_dir="report/save_data")
 
-
 features_index = data["features_index"]
 management = data["info_management"]
 
 df = features_index.merge(management, on="spell_id", how="left")
+
+stemi_nstemi = df[df["stemi_index"] | df["nstemi_index"]]
